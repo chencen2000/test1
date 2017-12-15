22d07a32d64d3830f9899fff8481e41a9f717086
Chris <chencen2000@gmail.com>
2017-12-14 22:13:31 -0800

    test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Specialized.StringDictionary _args = util.parseCommandLines(args);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class util
+    {
+        public static System.Collections.Specialized.StringDictionary parseCommandLines(String [] args)
+        {
+            System.Collections.Specialized.StringDictionary ret = new System.Collections.Specialized.StringDictionary();
+            foreach(string s in args)
+            {
+                if (s.StartsWith('-'))
+                {
+                    string k = s.Substring(1);
+                    string v = "";
+                    int pos = s.IndexOf('=');
+                    if (pos > 0)
+                    {
+                        k = s.Substring(1, pos - 1);
+                        if (pos + 1 < s.Length)
+                            v = s.Substring(pos + 1);
+                    }
+                    ret[k] = v;
+                }
+            }
+            return ret; 
+        }
+    }
+}
