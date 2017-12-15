a5521b56cb45ccb9bc16d74a20b7fe9494ec00a5
chencen2000 <chencen2000@gmai.com>
2017-12-15 11:31:27 -0800

    test nic

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,8 +7,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             System.Collections.Specialized.StringDictionary _args = util.parseCommandLines(args);
-            System.Tuple<Boolean,int,String[]> ret=util.runExe("cmd.exe", "/c dir");
+            //System.Tuple<Boolean,int,String[]> ret=util.runExe("cmd.exe", "/c dir");
             Console.WriteLine("Hello World!");
+            test();
+        }
+
+        static void test()
+        {
+            System.Net.NetworkInformation.NetworkInterface[] nics = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+            foreach(System.Net.NetworkInformation.NetworkInterface nic in nics)
+            {
+
+            }
         }
     }
 }
