35626ae26014f9c94de1326eee083609d9849caa
Chris Chen <cchen@futuredial.com>
2019-10-07 23:10:02 -0700

    Add py file.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 
-def test():
+def test_1():
     img = cv2.imread(r'C:\Projects\opencv\picture\test.jpg')
     template = cv2.imread(r'C:\Projects\opencv\picture\gold_2.png')
     w, h, c = template.shape
@@ -12,7 +12,13 @@ def test():
     loc = np.where(res >= threshold)
     for pt in zip(*loc[::-1]):
         cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    cv2.imwrite('res.png',img)
+    cv2.imwrite('res.png', img)
+
+
+def test():
+    img = cv2.imread(r'images\huahen.png', 0)
+    kernel = np.ones((3, 3), np.uint8)
+    gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
 
 
 if __name__ == '__main__':
